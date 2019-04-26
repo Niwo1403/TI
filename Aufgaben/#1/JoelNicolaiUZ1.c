efb6e9d19db568975666f51a5d4c206f2e1ecef8
Heuer-Joel <heuer.joel@hotmail.com>
2019-04-26 23:21:06 +0200

    Fertige 3Punkte Version.

--- Aufgaben/#1/JoelNicolaiUZ1.c
+++ Aufgaben/#1/JoelNicolaiUZ1.c
@@ -13,6 +13,9 @@
 // Maximum Character of one line
 #define MAXLINE 250
 
+int invalidFormat(char arr[]);	// checks invalid format
+int invalidFirstLine(char arr[]);	// checks invalid first Line
+
 int main(int argc, char *argv[]){
 
 	// Missing arguments
@@ -41,9 +44,15 @@ int main(int argc, char *argv[]){
 		fprintf(stderr, RED BOLD "ERROR: Read-Error\n");
 		return(1);
 	}
+	// invalid format?
+	if(invalidFirstLine(buffer)){
+		fprintf(stderr, RED BOLD "ERROR: Format-Error\n");
+		return(1);
+	}
+
 	// convert stringNumber to integer
 	number = strtoimax(buffer, NULL, 10);
-	printf("str: \t %s", buffer);
+	// printf("str: \t %s", buffer);
 	printf("int: \t %d\n\n", number);
 
 	// // Read till the end of file
@@ -56,6 +65,11 @@ int main(int argc, char *argv[]){
 
 		if(check == NULL){
 			fprintf(stderr, RED BOLD "ERROR: Read-Error\n");
+			return(1);
+		}
+
+		if(invalidFormat(buffer)){
+			fprintf(stderr, RED BOLD "ERROR: Format-Error\n");
 			return(1);
 		}
 
@@ -85,3 +99,65 @@ int main(int argc, char *argv[]){
 	return(0);
 }
 
+/*	first line is valid <-> contains only digits 
+	and nothing else
+	OR
+	<-> empty <-> 
+*/
+int invalidFirstLine(char arr[]){
+	for(unsigned int i = 0; i < strlen(arr); ++i){
+		if((48 <= arr[i] && arr[i] <= 57) ||
+			(i != 0 && arr[i] =='\n') ) continue;
+		else if(arr[i] == '\0') break;
+		else return(1);
+	}
+	return(0);
+}
+
+
+
+/*	format is valid
+		1) chars are digits and '*' WITHOUT \n or \t
+		2) only one '*' exists
+		3) first and last entry is digit and not a '*'
+*/
+int invalidFormat(char arr[]){ 
+	int starCounter = 0;	// counter for '*'
+	int flagA = 0; 			// exists a of a*b?
+	int flagB = 0; 			// exists b of a*b?
+
+
+	for(unsigned int i = 0; i < strlen(arr); ++i){
+		// ENTER
+		if(arr[i] == '\n'){
+			// invalid #'*'
+			if(starCounter != 1 || !flagA || !flagB) 
+				return(1);
+			// it is still possible that this exists: 121*\n
+			else if(i != 0 && arr[i-1] == 42) 
+				return(1);
+			// ok
+			else 	return(0);
+		}
+		// 1) digits
+		else if(48 <= arr[i] && arr[i] <= 57){
+				if(!flagA)			flagA = 1;
+				else if(!flagB) 	flagB = 1;
+				
+				continue;
+		} 
+		// 1) '*'
+		else if(42 == arr[i]){
+			starCounter++;
+			// 2) & 3)
+			if(starCounter > 1 || i == 0)
+				return(1);
+		} 
+		else if(arr[i] == '\0') return(0);
+		// other invalid chars
+		else return(1);
+	}
+
+	// irrelevant
+	return(0);
+}
