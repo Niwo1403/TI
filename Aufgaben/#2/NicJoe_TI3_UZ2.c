641bf29de7be5e08ea9eb76b5e845b797b6a7089
Heuer-Joel <heuer.joel@hotmail.com>
2019-04-28 02:03:21 +0200

    Fast fertig

--- Aufgaben/#2/NicJoe_TI3_UZ2.c
+++ Aufgaben/#2/NicJoe_TI3_UZ2.c
@@ -1,60 +1,50 @@
+/*	Nicolai Wolfrom & Joel Heuer 
+	TI III - Uebung #2 Programmierung
+
+
+
+	TO DO:
+		1) 	Gescheite Fehlermeldungen + Begruendung 
+			ohne <stdio.h> & <stdlib.h>
+
+		2) 	Ordner trashcan unsichtbar machen koennen
+
+		3) 	LIST-Funktion kann bis jetzt noch nix auflisten
+			weil wir prinf() nicht nutzen ḱoennen
+*/
+
 #include <sys/stat.h>	// mkdir & open
 #include <sys/types.h>	// mkdir
 #include <sys/stat.h>	// open
 #include <fcntl.h>		// open
-#include <unistd.h> // write, unlink, read
+#include <unistd.h> 	// write, unlink, read
+#include <libgen.h>		// dirname
+#include <string.h>		// strcat, strncpy, strcmp
+#include <dirent.h> 	// readdir
 
-#define PUFFERSIZE 10	// groeße des Puffer-Arrays fuer Read
-
-/*
-int mkdir(const char *pathname, mode_t mode);
-
-int open(const char *pathname, int flags);
-int open(const char *pathname, int flags, mode_t mode);
-
-ssize_t read (int fd, void * buf, size_t count);
-
-int unlink (const char * Pfadname );
-
-ssize_t write(int fd, const void *buf, size_t count);
-
-*/
-
+#define PUFFERSIZE 10	// groeße des Puffer-Arrays fuer Read in copy()
+#define PATHSIZE 100	// große fuer Array, das Pfad speichert
 
 // FÜR DEBUGGING
-#include <stdio.h>
-// spaeter loeschen
+// #include <stdio.h>
 
+// FUNKTIONEN
 int copy(char *sourcename, char *targetname);
-int trashcan();
+int trashcan(char *mode, char *file);
+int myStrlen(char p[]);
 
+// MAIN
+int main(int argc, char *argv[]){
 
-int main(void){
+	int  a;
 
-	// char buf[10];
+	if(argc < 2) 		return(1);
+	else if(argc == 2)  a = trashcan(argv[1], "/");
+	else 				a = trashcan(argv[1], argv[2]);
 
-	// int filedesc = open("testfile.txt",
-	// 				    O_CREAT | O_RDONLY, 0600);
-	// 				   // S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
- //    if(filedesc < 0){
- //    	fprintf(stderr, "fail\n");
- //        return 1;
- //    }
- //    fprintf(stdout, "Try to read\n");
- //    int a = read(filedesc, buf, 3);
- //    printf("%s\n", buf);
- //    close(filedesc);
+	// if(a) printf("Fehler\n");
 
-	char *source = "source.txt";
-	char *target = "copy.txt";
-
-	int check = copy(source, target);
-	if(check){
-		printf("ERROR: COPY\n");
-		return(1);
-	}
-
-    return(0);
+    return(a);
 }
 
 int copy(char *sourcename, char *targetname){
@@ -65,12 +55,12 @@ int copy(char *sourcename, char *targetname){
 	int checkRead = 0, checkWrite = 0; 	// wie viele Bytes wurden gelesen/geschrieben
 
 	// pruefen, ob Quelle existiert
-	printf("%s\n", sourcename);
+	// printf("%s\n", sourcename);
 	fdSource = open(sourcename, O_RDONLY);
 	if(fdSource < 0) return(1); 
 
 	// pruefen, ob Ziel schon existiert
-	printf("%s\n", targetname);
+	// printf("%s\n", targetname);
 	fdTryTarget = open(targetname, O_WRONLY);
 	// falls nein, Datei erstellen
 	if(fdTryTarget < 0){
@@ -79,14 +69,11 @@ int copy(char *sourcename, char *targetname){
 			// Quelle lesen 
 			checkRead = read(fdSource, buffer, PUFFERSIZE);
 			if(checkRead < 0) return(1);	// ERROR
-			// fprintf(stdout, "seen: %d\n", checkRead);
 			else if(checkRead == 0) break;	// Falls am Ende der Datei, stopppe
 			do{							// sonst in Ziel hineinschreiben
 				// so viel schreiben, wie gelesen wurde
 				checkWrite += write(fdTarget, buffer, checkRead);	
-				// fprintf(stdout, "written: %d\n", checkWrite);
 			}while(checkRead != checkWrite);
-
 			checkRead = 0;
 			checkWrite = 0;
 		}
@@ -99,9 +86,109 @@ int copy(char *sourcename, char *targetname){
 	// falls ja, beende 
 	return(1);
 }
+// MAIN-ENDE
 
-int trashcan(){		// return 1 == FAIL,	return 0 == SUCCESS
+int trashcan(char *mode, char *file){	// return 1 == FAIL,	return 0 == SUCCESS
+	char *trashcanName = ".trashcan";
+	int check;	// fuer mkdir, memset (erfolgreich?)
 
+	// Papierkorb erstellen, falls nicht vorhanden
+	// mit folgenden Berechtigungen: Lesen, Schreiben, Suchen [Besitzer & Gruppe]
+	// 								 Lesen, suchen 			  [Andere]
+	check = mkdir(trashcanName, 0755 /*| S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH*/);
+	// if(check == -1) return(1);	// Ordner existiert schon
 
+	// CHECK FOR VALID FILE-NAME
+		// --> "/" is invalid filename
+
+	// aktuelles Verzeichnis bzw. Path
+	char path[PATHSIZE];
+    if (getcwd(path, sizeof(path)) == NULL) // Fehler
+    	return(1);	
+
+    // printf("%s, %s\n", mode, file);
+	// DELETE
+	if(strcmp(mode, "-d") == 0 && strcmp(file, "/") != 0){
+		 // Path-Wechsel: Papierkorb
+	    strcat(path, "/");
+	    strcat(path, trashcanName);
+
+		// Was soll in den Papierkorb?
+		strcat(path, "/");
+		strcat(path, file);
+	
+		// kopiere in den Papierkorb
+		copy(file, path);
+
+		// loesche original
+		check = unlink(file);
+		if(check == -1) return(1);
+		return(0);
+	}
+	// LIST
+	else if(strcmp(mode, "-l") == 0 && strcmp(file, "/") == 0){
+    	
+    	struct dirent *dir;
+		DIR *directory;
+
+   		 // Path-Wechsel: Papierkorb
+	    strcat(path, "/");
+	    strcat(path, trashcanName);
+	    
+	    // Ordner oeffnen
+    	directory = opendir(path);
+    	if(directory != 0) return(1);	// Fehler
+    	
+    	// solange auslesen, bis alle Dateien ausgelesen
+    	while ((dir = readdir(directory)) != NULL){
+            // printf("%s\n", dir->d_name);
+        }
+        closedir(directory);
+    	return(0);
+	}
+
+	// RECOVER && FINALLY DELETE
+	else if((strcmp(mode, "-r") == 0 || strcmp(mode, "-f") == 0)
+			&& strcmp(file, "/") != 0){
+		// kopiere aktuellen Pfad
+		char pathTrashcan[PATHSIZE];
+		strncpy(pathTrashcan, path, myStrlen(path));
+
+		// Path-Kopie: gehe in den Papierkorb
+		strcat(pathTrashcan, "/");
+		strcat(pathTrashcan, trashcanName);
+		strcat(pathTrashcan, "/");
+		strcat(pathTrashcan, file);
+		// printf("TRASHCAN: \t %s\n", pathTrashcan);
+
+		// pruefen, ob Datei existiert
+		check = open(pathTrashcan, O_RDONLY);
+		if(check < 0) return(1);
+
+		// FUER RECOVOVER
+		if(strcmp(mode, "-r") == 0){
+			// Datei existiert 
+			// Datei im Verzeichnis wiederherstellen
+			close(check);
+			strcat(path, "/"); strcat(path, file);
+			// printf("PATH \t\t %s\n", path);
+			copy(pathTrashcan, path);
+		}
+		// RECOVER ENDE
+
+		// und im Papierkorb loeschen
+		unlink(pathTrashcan);
+		return(0);
+	}
+	
 	return(1);
+}
+
+int myStrlen(char p[]){
+	int len = 0;
+	
+	for(int i = 0; p[i] != '\0'; ++i){
+		len++;
+	}
+	return(len+1);
 }
